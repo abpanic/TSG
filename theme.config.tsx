e26192ab2e43659d5e4f900ccf49ea0569e7486f
Abhilash <abhilash.panicker@outlook.com>
2024-03-05 21:06:15 +0530

    name change
    -added my name
    - added author attribute

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>D365 CE TSG</span>,
   project: {
-    link: 'https://github.com/',
+    link: 'https://github.com/abpanic',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
