9650838e6cb77549f45e826473adf12c02f93f71
Abhilash <abhilash.panicker@outlook.com>
2024-03-08 05:47:40 +0530

    introduced frontmatter

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>D365 CE TSG</span>,
@@ -7,9 +9,55 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/abpanic',
   },
   docsRepositoryBase: 'https://github.com/abpanic/TSG',
-  footer: {
-    text: 'D365 CE Customization Troublshooting Guide',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s'
+      }
+    }
   },
+  themeSwitch: {
+    useOptions() {
+      return {
+        light: 'Light',
+        dark: 'Dark',
+        system: 'System'
+      }
+    }
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://customization-tsg.vercel.app/" target="_blank">
+          D365 CE Customization Support Team 
+        </a>
+        .
+      </span>
+    )
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://my-app.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+  
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Nextra'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'The next site builder'}
+        />
+      </>
+    )
+  }
 }
 
 export default config
+
+
+
